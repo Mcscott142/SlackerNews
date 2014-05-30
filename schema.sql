415f9711c6681d99ad42cbc19913b30da3a8465e
Colin Scott <m.colin.scott@gmail.com>
2014-05-30 14:34:27 -0400

    core and noncore validation missing comments

--- schema.sql
+++ schema.sql
@@ -2,7 +2,8 @@ CREATE TABLE articles(
 id serial PRIMARY KEY,
 title varchar(500) NOT NULL,
 url varchar(1000) NOT NULL,
-description varchar(1000)
+description varchar(1000),
+created_at TIMESTAMP NOT NULL
 );
 
 CREATE TABLE comments(
--- server.rb
+++ server.rb
@@ -3,26 +3,42 @@ require 'pry'
 #require 'csv'
 require 'pg'
 
-def db_connection
-  begin
-    connection = PG.connect(dbname: 'slacker_news')
-  yield(connection)
-  ensure
-    connection.close
-  end
-end
+# def db_connection
+#   begin
+#     connection = PG.connect(dbname: 'slacker_news')
+#   yield(connection)
+#   ensure
+#     connection.close
+#   end
+# end
 
 def find_articles
   connection = PG.connect(dbname: 'slacker_news')
-  results = connection.exec('SELECT * FROM articles')
+  results = connection.exec('SELECT * FROM articles ORDER BY created_at DESC')
   connection.close
 
   results
 end
 
+def find_urls
+  urls= []
+  connection = PG.connect(dbname: 'slacker_news')
+  results = connection.exec('SELECT url FROM articles')
+  connection.close
+
+  results.each do |result|
+    result.each do |k, v|
+    urls << v
+    end
+  end
+
+  urls
+
+end
+
 def save_article(title, url, description)
-  sql = "INSERT INTO articles (title, url, description) " +
-    "VALUES ($1, $2, $3)"
+  sql = "INSERT INTO articles (title, url, description, created_at) " +
+    "VALUES ($1, $2, $3, NOW())"
 
   connection = PG.connect(dbname: 'slacker_news')
   connection.exec_params(sql, [title, url, description])
@@ -31,7 +47,7 @@ end
 
 get '/articles' do
   @articles = find_articles
-#binding.pry
+
   erb :'/articles/index'
 end
 
@@ -42,12 +58,18 @@ get '/articles/new' do
 end
 
 post '/articles' do
-# title = params["title"]
-# url = params["url"]
-# description= params["description"]
- #binding.pry
- save_article(params["title"],params["url"], params["description"])
+@errors =[]
+@urls = find_urls
 
-redirect '/articles'
+  if !@urls.include?(params["url"])
+      save_article(params["title"],params["url"], params["description"])
+      redirect '/articles'
 
-end
+    else
+      @errors << "Site already Posted!"
+
+      erb :'articles/show'
+    end
+  end
+
+
